b1fff1aa76fd44674d1740f3023eab4d83309d51
Artem Kostiuk <postatum@gmail.com>
2017-05-15 16:20:33 +0300

    Trying to make java example work

--- demos/java/src/main/java/javatogo/JavaToGo.java
+++ demos/java/src/main/java/javatogo/JavaToGo.java
@@ -1,10 +1,25 @@
 package javatogo;
 
 import java.io.File;
+import java.util.List;
+import java.util.Arrays;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Platform;
+import com.sun.jna.Structure;
+
+class GoString extends Structure {
+  public String p;
+  public long n;
+  protected List<String> getFieldOrder() {
+      return Arrays.asList(new String[] { "p", "n" });
+  }
+  public GoString(String x, long y) {
+    p = x;
+    n = y;
+  }
+}
 
 public class JavaToGo {
   static MyLib MY_LIB;
@@ -25,7 +40,7 @@ public class JavaToGo {
   }
 
   public interface MyLib extends Library {
-    long PrintGoStr(String x);
+    long PrintGoStr(GoString x);
     long PrintInt(long x);
     long PrintCStr(String x);
     long PrintStruct(String x);
@@ -40,8 +55,9 @@ public class JavaToGo {
     System.out.println(MY_LIB.GetFirstJSONElement(pth));
 
     // These aren't working properly
+    GoString gs = new GoString("hi", 2L);
+    MY_LIB.PrintGoStr(gs);
     MY_LIB.PrintStruct("42");
     MY_LIB.PrintStructMeth("42");
-    MY_LIB.PrintGoStr("Hi to Go from Java");
   }
 }
